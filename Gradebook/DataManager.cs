3a3c9b8b4c6ac06cf400a018e60d47e3f3ec3fbb
Tarik Onalan <phi.quantum@gmail.com>
2015-01-08 22:00:05 -0800

    Initial commit

---
+++ Gradebook/DataManager.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+
+namespace Gradebook
+{
+    class DataManager
+    {
+        public static JsonObject Cache;
+
+        public enum StatusCode
+        {
+            SUCCESS = 0,
+            FAILURE = 1
+        }
+
+        public readonly string Username;
+        public readonly string Password;
+
+        public DataManager(string username, string password)
+        {
+            this.Username = username;
+            this.Password = password;
+        }
+
+        public async Task<StatusCode> Fetch(object sender, object e)
+        {
+            string url = "https://gradebook-web-api.herokuapp.com/?username=" + this.Username + "&password=" + this.Password;
+            var response = await GetWebResponse(url);
+
+            if (response.ContainsKey("error"))
+            {
+                return StatusCode.FAILURE;
+            }
+            else
+            {
+                Cache = response;
+                return StatusCode.SUCCESS;
+            }
+        }
+
+        private static async Task<JsonObject> GetWebResponse(string url)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.Accept = "application/json";
+
+            using (var response = (HttpWebResponse)(await request.GetResponseAsync()))
+            {
+                return JsonObject.Parse(new StreamReader(response.GetResponseStream()).ReadToEnd());
+            }
+        }
+    }
+}
---
+++ Gradebook/LoginPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Gradebook.Resources;
+using System.Windows.Media;
+
+namespace Gradebook
+{
+    public partial class LoginPage : PhoneApplicationPage
+    {
+        private readonly SolidColorBrush brush = new SolidColorBrush();
+
+        // Constructor
+        public LoginPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void UserBox_GotFocus(object sender, EventArgs e)
+        {
+            var textbox = sender as TextBox;
+            textbox.Text = "";
+        }
+
+        private void UserBox_GotFocus_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UserBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
