c0a13ad27626ae61e40fb9044bb30ff79d67dcfa
Tarik Onalan <phi.quantum@gmail.com>
2015-01-10 18:51:49 -0800

    Migrated to windows 8.1 univeral app structure

---
+++ Gradebook/Gradebook.Shared/DataManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+
+namespace Gradebook
+{
+    static class DataManager
+    {
+        public static JsonObject Cache;
+
+        public enum StatusCode
+        {
+            SUCCESS = 0,
+            FAILURE = 1
+        }
+
+        public static async Task<StatusCode> FetchAsync(string username, string password)
+        {
+            string url = "https://gradebook-web-api.herokuapp.com/?username=" + username + "&password=" + password;
+            var response = await GetWebResponse(url);
+
+            if (response.ContainsKey("error"))
+            {
+                return StatusCode.FAILURE;
+            }
+            else
+            {
+                Cache = response;
+                return StatusCode.SUCCESS;
+            }
+        }
+
+        private static async Task<JsonObject> GetWebResponse(string url)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.Accept = "application/json";
+
+            var response = (HttpWebResponse)(await request.GetResponseAsync());
+            return JsonObject.Parse(new StreamReader(response.GetResponseStream()).ReadToEnd());
+        }
+    }
+}
---
+++ Gradebook/Gradebook.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Gradebook
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public static SolidColorBrush brush = new SolidColorBrush();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        async void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            Element.Opacity = 0.5;
+            Progress.IsActive = true;
+            var statusCode = await DataManager.FetchAsync(UserBox.Text, PassBox.Password);
+            Progress.IsActive = false;
+            Element.Opacity = 1;
+            if (statusCode == DataManager.StatusCode.SUCCESS)
+            {
+                this.Frame.Navigate(typeof(GradePage));
+            }
+            else
+            {
+                brush.Color = Colors.Red;
+                PassBox.Password = "";
+                UserBox.BorderBrush = brush;
+            }
+        }
+    }
+}
